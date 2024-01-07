0563ac75cfef1def925221047388dc63cda41bc9
NZEHAKUJOBI <nzehdaniel@gmail.com>
2024-01-07 15:40:07 +0100

    completed

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,20 +10,23 @@ function Login() {
         <div className='form_container p-5 rounded bg-white'>
             <form>
                 <h3 className="text-center">Sign In</h3>
-                <div className='mb-2'>
+                <div className="mb-2">
                     <label htmlFor="email">Email</label>
                     <input type ="email" placeholder='Enter your email address' className='form-control'/>
                 </div>
-                <div className='mb-2'>
+                <div className="mb-2">
                     <label htmlFor="password">Password</label>
                     <input type ="password" placeholder='Enter your password' className='form-control'/>
                 </div> 
-                <div className='mb-2'>
+                <div className="mb-2">
                     <input type='checkbox' className='custom-control custom-checkbox' id ="check"/>
                     <label htmlFor='check' className='custom-input-label ms-2'>
                         Remember me
                     </label>
 
+                </div>
+                <div className="d-grid">
+                    <button className='btn btn-primary'>Sign in </button>
                 </div>
                 <p className="text-end mt-2">
                     Forget <a href=''>Password? </a><Link to='/signup' className='ms-2'>  SignUp </Link>
